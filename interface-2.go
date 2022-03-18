fcf5a22665f95a6fbaed144bd987460b63b2e1ef
mahdi-laptop <imani.mahdi@gmail.com>
2022-03-18 12:13:24 +0330

    update interface

---
+++ interface-2.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+var key = "name"
+
+func main() {
+	list := []interface{}{"first"}
+	list = append(list, string("second"))
+
+	Application(list)
+}
+
+func Application(list []interface{}) {
+	user := map[string]interface{}{}
+	user[key] = "ali"
+	list = append(list, user)
+
+	userTwo := map[string]interface{}{}
+	userTwo[key] = "sajad"
+	list = append(list, userTwo)
+
+	userThree := struct {
+		Name   string
+		Family string
+	}{
+		Name:   "Mahdi",
+		Family: "Imani",
+	}
+	list = append(list, userThree)
+
+	fmt.Println(list...)
+
+	printInterfaceName(list)
+}
+
+func printInterfaceName(list []interface{}) {
+	for index, _ := range list {
+		fmt.Println(list[index])
+	}
+}
