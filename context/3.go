a9fb91ca39344f5e8de87d9ca5dbe451f1781433
mahdi <imani.mahdi@gmail.com>
2023-03-03 20:03:26 +0300

    feat: update context staffs

--- context/3.go
+++ context/3.go
@@ -8,24 +8,35 @@ import (
 )
 
 // https://www.sohamkamani.com/golang/context-cancellation-and-values/
+
+//
+// Scenario:
+// -------------------------------------
+//    curl http://localhost:8000
+//    after 2 seconds perform CANSELATION by using "ctl+c"
+//
 func main() {
+	// Total pocessing time for completing single Request
+	const operationDelay = 5 * time.Second
 	// Create an HTTP server that listens on port 8000
 	http.ListenAndServe(":8000", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		// This prints to STDOUT to show that processing has started
-		fmt.Fprint(os.Stdout, "processing request\n")
+		fmt.Fprint(os.Stdout, "[INFO] Start processing new request\n")
 		// We use `select` to execute a piece of code depending on which
 		// channel receives a message first
 		select {
-		case <-time.After(5 * time.Second):
+		case <-time.After(operationDelay):
 			// If we receive a message after 2 seconds
 			// that means the request has been processed
 			// We then write this as the response
-			w.Write([]byte("request processed"))
+			w.Write([]byte("{\"status\":\"request processed\"}"))
+			fmt.Fprint(os.Stdout, "[INFO] Responce sended \n")
 		case <-ctx.Done():
 			// If the request gets cancelled, log it
 			// to STDERR
-			fmt.Fprint(os.Stderr, "request cancelled\n")
+			fmt.Fprint(os.Stderr, "[ERROR] Request cancelled\n")
 		}
+		fmt.Fprint(os.Stdout, "[INFO] FINISH \n")
 	}))
 }
--- context/4.go
+++ context/4.go
@@ -6,33 +6,31 @@ import (
 	"time"
 )
 
-func operation(ctx context.Context) {
-	// We use a similar pattern to the HTTP server
-	// that we saw in the earlier example
-	select {
-	case <-time.After(10 * time.Millisecond):
-		fmt.Println("done")
-	case <-ctx.Done():
-		fmt.Println("halted operation2")
-	}
+// heavy process function
+func fn(ctx context.Context, c chan context.Context) {
+	// ctx1, _ := context.WithTimeout(ctx, 2*time.Second)
+	time.Sleep(1 * time.Second)
+	ctx.Done()
+	c <- ctx
+	// return ctx.Done()
 }
 
 func main() {
-	// Create a new context
+	c := make(chan context.Context)
 	ctx := context.Background()
-
-	// Create a new context, with its cancellation function
-	// from the original context
-	ctx, cancel := context.WithCancel(ctx)
-
-	// Run operation
-	operation(ctx)
-
-	// cancel next operation
-	if true {
-		cancel()
+	context.WithValue(ctx, "rid", "9f44f4ba-f84e-11ec-9c6d-6fc79d5e25bf")
+	go fn(ctx, c)
+	for {
+		time.Sleep(500 * time.Millisecond)
+		select {
+		case <-c:
+			fmt.Println("Context finished using channel")
+		case <-ctx.Done():
+			fmt.Println("Context finished")
+			return
+		default:
+			fmt.Println("-")
+		}
 	}
-
-	// Run operation
-	operation(ctx)
+	fmt.Println(ctx.Value("rid"))
 }
