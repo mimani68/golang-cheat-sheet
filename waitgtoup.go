28fe766c423466ce87a9d95d7b8d2044865cf27b
dev <imani.mahdi@gmail.com>
2023-03-27 13:13:24 +0330

    feat: update waitgroup

--- waitgtoup.go
+++ waitgtoup.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
-var wg sync.WaitGroup
-
-func hello() {
+func getBunchOfData(wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Println("hello from a goroutine!")
+	fmt.Println("Start fetch data")
+	time.Sleep(time.Second * 2)
+	fmt.Println("Data gathered from source")
 }
 
 func main() {
-	wg.Add(1)
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
 	defer wg.Wait()
-	go hello()
+	go getBunchOfData(wg)
+	time.Sleep(time.Millisecond * 300)
+	go getBunchOfData(wg)
 }
