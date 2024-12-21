d179802363752e76ae58f147177f0af7cd3cff3e
Matt <imani.mahdi@jinz.ai>
2024-12-21 12:08:10 +0330

    feat: update pointer values

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -5,18 +5,27 @@ import "fmt"
 func main() {
 
 	a := "Hey"
+	fmt.Println("a")
 	fmt.Println("show value: ", a)                                 // "Hey"
-	fmt.Println("show memory addess of varable: ", &a)             // 0xc000010240
+	fmt.Println("show memory address of variable: ", &a)           // 0xc000010240
 	fmt.Println("find memory address, then return value: ", *(&a)) // "Hey"
 
-	b := &struct {
+	b := new(string)
+	*b = "hey"
+	fmt.Println("b")
+	fmt.Println("show value: ", *b)                     // "hey"
+	fmt.Println("show memory address of variable: ", b) // 0xc0000140b0
+	fmt.Println("find memory address: ", &b)            // 0xc00005c048
+
+	c := &struct {
 		Name string
 	}{
 		Name: "Ali",
 	}
-	fmt.Println("show value: ", b)                                 // {Ali}
-	fmt.Println("show memory addess of varable: ", &b)             // 0xc0000b6020
-	fmt.Println("find memory address, then return value: ", *b)    // {Ali}
-	fmt.Println("find memory address, then return value: ", *(&b)) // &{Ali}
+	fmt.Println("c")
+	fmt.Println("show value: ", c)                                 // {Ali}
+	fmt.Println("show memory address of variable: ", &c)           // 0xc0000b6020
+	fmt.Println("find memory address, then return value: ", *c)    // {Ali}
+	fmt.Println("find memory address, then return value: ", *(&c)) // &{Ali}
 
 }
